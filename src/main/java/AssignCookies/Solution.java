9441a5af21f1491aad0b9e8f093212c7f7d25135
ichathux <ichathux@gmail.com>
2022-10-28 22:30:16 +0530

    not done Assign cookies

---
+++ src/main/java/AssignCookies/Solution.java
@@ -0,0 +1,23 @@
+package AssignCookies;
+
+import java.util.ArrayList;
+
+// TODO: 2022-10-28  
+public class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+
+        int temp = 0;
+
+        for(int i = 0; i < g.length; i++){
+            try{
+                if(g[i] <= s[i]){
+                    temp = Math.max (s[i],temp);
+                }
+            }catch (ArrayIndexOutOfBoundsException e){
+                break;
+            }
+
+        }
+        return temp;
+    }
+}
